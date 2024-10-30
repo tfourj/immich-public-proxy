d05296509dec5803e24adc4971bf84f3e181d556
Alan Grainger <alan.grainger@gmail.com>
2024-10-30 12:45:23 +0100

    Update docs

--- src/render.ts
+++ src/render.ts
@@ -17,6 +17,13 @@ class Render {
     }
   }
 
+  /**
+   * Render a gallery page for a given SharedLink, using EJS and lightGallery.
+   *
+   * @param res - ExpressJS Response
+   * @param share - Immich `shared-link` containing the assets to show in the gallery
+   * @param [openItem] - Immediately open a lightbox to the Nth item when the gallery loads
+   */
   async gallery (res: Response, share: SharedLink, openItem?: number) {
     const items = []
     for (const asset of share.assets) {
