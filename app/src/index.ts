4d563d69aa9b7971a4edbe8da37ca5e538152b72
Alan Grainger <alan.grainger@gmail.com>
2024-11-15 16:01:10 +0100

    Whitelist mode

--- app/src/index.ts
+++ app/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.static('public', { setHeaders: addResponseHeaders }))
 /*
  * [ROUTE] This is the main URL that someone would visit if they are opening a shared link
  */
-app.get('/share/:key/:mode?', async (req, res) => {
+app.get('/share/:key/:mode(download)?', async (req, res) => {
   await immich.handleShareRequest({
     key: req.params.key,
     mode: req.params.mode
